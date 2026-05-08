65b92af2d9ddbfab2c62753528876b370d0634c1
kutharmanvith-tech <kutharmanvith@gmail.com>
2026-05-08 15:03:45 +0530

    Implement logic to find the smallest number
    Fix logic to find the smallest number in the array.

---
+++ smallestnum.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int arr[]={10,20,35,353,1,67};
+        int largest=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<largest){
+                largest=arr[i];
+            }
+        }
+        System.out.println(largest);
+     }
+}
