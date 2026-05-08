c8c482003024ca3355697f4b06a8c2c1a47601a6
kutharmanvith-tech <kutharmanvith@gmail.com>
2026-05-08 14:44:36 +0530

    Add demo class to calculate sum of an array

---
+++ demo.java
@@ -0,0 +1,13 @@
+class demo{
+    public static void main(String[]args)
+    {
+        int []k={1,2,3,4};
+        int sum=0;
+        for(int i=0;i<k.length;i++)
+        {
+            sum+=k[i];
+        }
+        System.out.println(sum);
+        
+    }
+}
