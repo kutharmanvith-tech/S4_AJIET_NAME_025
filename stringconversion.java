a40bcc03cb0222cbe52c58fe8e795706c5ae46e1
kutharmanvith-tech <kutharmanvith@gmail.com>
2026-05-08 15:49:28 +0530

    Add string conversion operations in stringconversion.java

---
+++ stringconversion.java
@@ -0,0 +1,13 @@
+class stringconversion{
+    public static void main(String[] args) {
+        String a="Manvith";
+        String b="Man";
+        System.out.println(a);
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.indexOf("an"));
+        System.out.println(a.charAt(3));
+        System.out.println(a.equals(b));
+    }
+}
+        
