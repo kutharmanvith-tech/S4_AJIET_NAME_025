64c21264ec85527f2abec107a318259bed113cf2
kutharmanvith-tech <kutharmanvith@gmail.com>
2026-05-09 13:48:15 +0530

    Implement student ranking logic in studentrank.java

---
+++ studentrank.java
@@ -0,0 +1,14 @@
+class main{
+    public static void main(String[]args){
+        int arr[]={43,65,23,54,3,55};
+        int register=arr[0];
+        int cut=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<register){
+                cut++;
+                register=arr[i+1];
+            }
+        }
+        System.out.println(cut);
+    }
+}
